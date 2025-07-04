a13a9c81d0276b072c6348cd9d1d8c31bd7e78ba
ToriRuban <toriruban@icloud.com>
2025-07-05 01:46:46 +0300

    feat(note-preview-client): use React Query and Modal for note details

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,30 +1,39 @@
 'use client'
 
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { Note } from '@/types/note';
 import css from './NotePreview.module.css';
+import { useQuery } from '@tanstack/react-query';
+import { fetchNoteById } from '@/lib/api';
+import Modal from '@/components/Modal/Modal';
 
-type Props = { note: Note }
-
-const NotePreviewClient = ({ note }: Props) =>{
-const router = useRouter();
-const handleBack = () => {
-    router.back();
-};
-return (
-    <div className={css.container}>
-      <button className={css.backBtn} onClick={handleBack}>
-        ← Back
-      </button>
-      <div className={css.header}>
-        <h2>{note.title}</h2>
-        <p className={css.date}>{new Date(note.createdAt).toLocaleString()}</p>
-      </div>
-      <p className={css.content}>{note.content}</p>
-      <span className={css.tag}>{note.tag}</span>
-    </div>
+const NotePreviewClient = () =>{
+  const { id } = useParams();
+  const NoteId = Number(id)
+  const router = useRouter();
+  const { data: note, isLoading, isError } = useQuery<Note>({
+    queryKey: ['note', NoteId],
+    queryFn: () => fetchNoteById(NoteId)
+  });
+  const handleClose = () => router.back();
+  return (
+    <Modal onClose={handleClose}>
+      {isLoading && <p className={css.message}>Loading note…</p>}
+      {isError && <p className={css.errorMessage}>Failed to load note.</p>}
+         { note && 
+           (<div className={css.container}>
+            <button className={css.backBtn} onClick={handleClose}>
+              ← Back
+            </button>
+            <div className={css.header}>
+              <h2>{note.title}</h2>
+              <p className={css.date}>{new Date(note.createdAt).toLocaleString()}</p>
+            </div>
+            <p className={css.content}>{note.content}</p>
+            <span className={css.tag}>{note.tag}</span>
+          </div>)
+          }
+    </Modal>
 )
 }
-
-
 export default NotePreviewClient;
