b45bfcb3e9f2819d381a5d455fcb8ca659027c79
ToriRuban <toriruban@icloud.com>
2025-07-05 01:27:19 +0300

    feat(note-preview): prefetch and hydrate note data in NotePreviewPage

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { fetchNoteById } from '@/lib/api';
 import Modal from '@/components/Modal/Modal';
 import NotePreviewClient from './NotePreview.client';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 type Props = {
     params: Promise<{ id: string }>
@@ -8,12 +9,20 @@ type Props = {
 
 const NotePreviewPage = async ({ params } : Props) => {
     const { id } = await params;
-    const note = await fetchNoteById(id)
+    const noteId = Number(id);
+    const note = await fetchNoteById(noteId);
+    const queryClient = new QueryClient();
+    queryClient.prefetchQuery({
+        queryKey: ['note', noteId],
+        queryFn: () => fetchNoteById(noteId)
+    })
    
     return(
+        <HydrationBoundary state={dehydrate(queryClient)}>
         <Modal>
             <NotePreviewClient note={note} />
         </Modal>
+        </HydrationBoundary>
     )
     
 }
